95b84a069f03e14f3056af78b9b698a9a163cd10
anandu <anandu@gamezop.co>
2019-07-19 11:23:17 +0530

    Day 19 |> grade school

---
+++ Day19/grade-school/school.exs
@@ -0,0 +1,35 @@
+defmodule School do
+  @moduledoc """
+  Simulate students in a school.
+
+  Each student is in a grade.
+  """
+
+  @doc """
+  Add a student to a particular grade in school.
+  """
+  @spec add(map, String.t(), integer) :: map
+  def add(db, name, grade) do
+    Map.update(db, grade, [name], fn list -> list ++ [name] end)
+  end
+
+  @doc """
+  Return the names of the students in a particular grade.
+  """
+  @spec grade(map, integer) :: [String.t()]
+  def grade(db, grade) do
+    case Map.fetch(db, grade) do
+      {:ok, val} -> val
+      :error -> []
+    end
+  end
+
+  @doc """
+  Sorts the school by grade and name.
+  """
+  @spec sort(map) :: [{integer, [String.t()]}]
+  def sort(db) do
+    Enum.reduce(db, %{}, fn {key, value},acc_map -> Map.put(acc_map, key, Enum.sort(value)) end)
+    |> Map.to_list
+  end
+end
