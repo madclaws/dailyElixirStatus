2767b550c745d807112895778ae8136d2653960c
anandu <anandu@gamezop.co>
2019-07-24 23:53:22 +0530

    Day24 |> beer song

--- Day24/beer-song/beer_song.exs
+++ Day24/beer-song/beer_song.exs
@@ -5,32 +5,38 @@ defmodule BeerSong do
   """
   @spec verse(integer) :: String.t()
   def verse(0) do
-    "No more #{get_bottle_of_beer(nil)} on the wall, no more #{get_bottle_of_beer(nil)}.\n" <>
-    "Go to the store and buy some more, 99 #{get_bottle_of_beer(99)} on the wall.\n"
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
   end
 
   def verse(1) do
-    "1 #{get_bottle_of_beer(1)} on the wall, 1 #{get_bottle_of_beer(1)}.\n" <>
-    "Take it down and pass it around, no more #{get_bottle_of_beer(nil)} on the wall.\n"
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
   end
 
-  def verse(number) when number > 1 and number <= 99 do
-    "#{number} #{get_bottle_of_beer(number)} on the wall, #{number} #{get_bottle_of_beer(number)}.\n" <>
-    "Take one down and pass it around, #{number - 1} #{get_bottle_of_beer(number - 1)} on the wall.\n"
+  def verse(2) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
   end
 
-  defp get_bottle_of_beer(number_of_bottles)
-  defp get_bottle_of_beer(1), do: "bottle of beer"
-  defp get_bottle_of_beer(_), do: "bottles of beer"
+  def verse(number) when number > 2 and number <= 99 do
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    """
+  end
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range \\ 99..0) do
-    Enum.reduce(range, "", fn bottle_number, total_lyrics ->
-      total_lyrics <> "\n" <> verse(bottle_number)
-    end)
-    |> String.trim_leading
+    Enum.map_join(range, "\n", &(verse(&1)))
   end
 end
