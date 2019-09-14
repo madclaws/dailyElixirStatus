1a02dd89b098ba65f1510f9681912d7a4da3eeea
anandu <anandu@gamezop.co>
2019-09-14 23:54:58 +0530

    Day74 |> ISBN-VERIFIER

---
+++ Day74/isbn-verifier/lib/isbn_verifier.ex
@@ -0,0 +1,45 @@
+defmodule IsbnVerifier do
+  @doc """
+    Checks if a string is a valid ISBN-10 identifier
+
+    ## Examples
+
+      iex> ISBNVerifier.isbn?("3-598-21507-X")
+      true
+
+      iex> ISBNVerifier.isbn?("3-598-2K507-0")
+      false
+
+  """
+  @spec isbn?(String.t()) :: boolean
+  def isbn?(isbn) do
+    parsed_isbn = isbn
+                  |> String.split("-")
+                  |> Enum.join
+    validate_isbn(parsed_isbn, String.match?(parsed_isbn, ~r/^\d{10}$/),
+    String.match?(parsed_isbn, ~r/^\d{9}x$/))
+  end
+
+  defp validate_isbn(isbn, is_normal_matching, is_matching_x_check)
+  defp validate_isbn(isbn, true, _) do
+    compute_value = isbn |> compute_isbn()
+    rem(compute_value, 11) === 0
+  end
+  defp validate_isbn(isbn, _, true) do
+    compute_value = isbn |> String.trim_trailing("x") |> compute_isbn()
+    rem(compute_value, 11) === 0
+  end
+
+  defp validate_isbn(_isbn, false, false) do
+    false
+  end
+
+  defp compute_isbn(isbn) do
+    isbn
+    |> String.codepoints
+    |> Enum.reduce({0, 10}, fn code, {acc, multiplier} ->
+      result = String.to_integer(code) * multiplier
+      {acc + result, multiplier - 1}
+    end)
+  end
+end
